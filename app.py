1c876e8b2281168e5ca894cd08da62ed69e8cb4b
nitesh29ns <94296741+nitesh29ns@users.noreply.github.com>
2023-03-05 18:41:00 +0530

    add

--- app.py
+++ app.py
@@ -2,16 +2,16 @@ from flask import Flask, render_template, request, Response
 from camera import get_frame
 
 
-app=Flask(__name__)
+application=Flask(__name__)
 
-@app.route("/")
+@application.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/video_feed")
+@application.route("/video_feed")
 def video_feed():
     return Response(get_frame(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 if __name__ == "__main__":
-    app.run()
+    application.run()
